12417c794c812df5ba9346ed032792a9644a7041
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2017-10-25 18:49:18 +0200

    fixed config

--- src/frontend/environments/environment.beta.ts
+++ src/frontend/environments/environment.beta.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     backendApiBaseUrl: 'http://www.michas-ausflugstipps.de/mytbbeta/api/v1/',
-    tracksBaseUrl: 'http://www.michas-ausflugstipps.de/tracks/',
-    picsBaseUrl: 'http://www.michas-ausflugstipps.de/digifotos/'
+    tracksBaseUrl: 'http://www.michas-ausflugstipps.de/mytbbeta/api/assets/tracks/',
+    picsBaseUrl: 'http://www.michas-ausflugstipps.de/mytbbeta/api/static/pictures/'
 };
--- src/frontend/environments/environment.prod.ts
+++ src/frontend/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     backendApiBaseUrl: 'http://www.michas-ausflugstipps.de/mytb/api/v1/',
-    tracksBaseUrl: 'http://www.michas-ausflugstipps.de/tracks/',
-    picsBaseUrl: 'http://www.michas-ausflugstipps.de/digifotos/'
+    tracksBaseUrl: 'http://www.michas-ausflugstipps.de/api/assets/tracks/',
+    picsBaseUrl: 'http://www.michas-ausflugstipps.de/api/static/pictures/'
 };
