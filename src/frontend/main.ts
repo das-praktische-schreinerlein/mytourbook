35c562beffc19d9f0e736c91aa8673b61f893968
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2017-10-31 23:56:10 +0100

    fixed ie-issues improved layout

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -5,6 +5,8 @@ import {AppModule} from './app/app.module';
 
 if (environment.production) {
     enableProdMode();
+    // disable console
+    console.debug = function () {};
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
