d43d477b1fb933a564412a2cc43a7b7874f51ca0
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2017-12-30 20:06:09 +0100

    configure dataservice-adapter in backend.json

--- src/backend/modules/sdoc-dataservice.module.ts
+++ src/backend/modules/sdoc-dataservice.module.ts
@@ -5,14 +5,27 @@ import {SDocSolrAdapter} from '../shared/sdoc-commons/services/sdoc-solr.adapter
 import axios from 'axios';
 import * as fs from 'fs';
 import {HttpAdapter} from 'js-data-http';
-import {SDocSqlAdapter} from '../shared/sdoc-commons/services/sdoc-sql.adapter';
+import {SDocSqlMediadbAdapter} from '../shared/sdoc-commons/services/sdoc-sql-mediadb.adapter';
+import {SDocSqlMytbAdapter} from '../shared/sdoc-commons/services/sdoc-sql-mytb.adapter';
 
 export class SDocDataServiceModule {
     private static dataServices = new Map<string, SDocDataService>();
 
     public static getDataService(profile: string, backendConfig: {}, readOnly: boolean): SDocDataService {
         if (!this.dataServices.has(profile)) {
-            this.dataServices.set(profile, SDocDataServiceModule.createDataServiceSolr(backendConfig, readOnly));
+            switch (backendConfig['sdocDataStoreAdapter']) {
+                case 'SDocSolrAdapter':
+                    this.dataServices.set(profile, SDocDataServiceModule.createDataServiceSolr(backendConfig, readOnly));
+                    break;
+                case 'SDocSqlMediadbAdapter':
+                    this.dataServices.set(profile, SDocDataServiceModule.createDataServiceMediadbSql(backendConfig, readOnly));
+                    break;
+                case 'SDocSqlMytbAdapter':
+                    this.dataServices.set(profile, SDocDataServiceModule.createDataServiceMytbSql(backendConfig, readOnly));
+                    break;
+                default:
+                    throw new Error('configured sdocDataStoreAdapter not exist:' + backendConfig['sdocDataStoreAdapter']);
+            }
         }
 
         return this.dataServices.get(profile);
@@ -29,14 +42,18 @@ export class SDocDataServiceModule {
         const dataService: SDocDataService = new SDocDataService(dataStore);
 
         // configure solr-adapter
+        const solrConfig = backendConfig['SDocSolrAdapter'];
+        if (solrConfig === undefined) {
+            throw new Error('config for SDocSolrAdapter not exists');
+        }
         const options = {
-            basePath: backendConfig['solrCoreSDoc'],
+            basePath: solrConfig['solrCoreSDoc'],
             suffix: '&wt=json&indent=on&datatype=jsonp&json.wrf=JSONP_CALLBACK&callback=JSONP_CALLBACK&',
             http: axios,
             beforeHTTP: function (config, opts) {
                 config.auth = {
-                    username: backendConfig['solrCoreSDocReadUsername'],
-                    password: backendConfig['solrCoreSDocReadPassword']
+                    username: solrConfig['solrCoreSDocReadUsername'],
+                    password: solrConfig['solrCoreSDocReadPassword']
                 };
 
                 // Now do the default behavior
@@ -49,7 +66,7 @@ export class SDocDataServiceModule {
         return dataService;
     }
 
-    private static createDataServiceSql(backendConfig: {}, readOnly: boolean): SDocDataService {
+    private static createDataServiceMediadbSql(backendConfig: {}, readOnly: boolean): SDocDataService {
         // configure store
         const filterConfig: SDocTeamFilterConfig = new SDocTeamFilterConfig();
         const themeFilters: any[] = JSON.parse(fs.readFileSync(backendConfig['filePathThemeFilterJson'], { encoding: 'utf8' }));
@@ -60,21 +77,57 @@ export class SDocDataServiceModule {
         const dataService: SDocDataService = new SDocDataService(dataStore);
 
         // configure solr-adapter
+        const sqlConfig = backendConfig['SDocSqlMediadbAdapter'];
+        if (sqlConfig === undefined) {
+            throw new Error('config for SDocSqlMediadbAdapter not exists');
+        }
         const options = {
             knexOpts: {
-                client: 'mysql',
+                client: sqlConfig['client'],
                 connection: {
-                    host: 'localhost',
-                    user: 'root',
-                    password: '',
-                    database: 'mytb',
-                    port: '3306'
+                    host: sqlConfig['host'],
+                    user: sqlConfig['user'],
+                    password: sqlConfig['password'],
+                    database: sqlConfig['database'],
+                    port: sqlConfig['port']
                 }
             }
         };
-        const adapter = new SDocSqlAdapter(options);
+        const adapter = new SDocSqlMediadbAdapter(options);
         dataStore.setAdapter('http', adapter, '', {});
 
+        return dataService;
+    }
+
+    private static createDataServiceMytbSql(backendConfig: {}, readOnly: boolean): SDocDataService {
+        // configure store
+        const filterConfig: SDocTeamFilterConfig = new SDocTeamFilterConfig();
+        const themeFilters: any[] = JSON.parse(fs.readFileSync(backendConfig['filePathThemeFilterJson'], { encoding: 'utf8' }));
+        for (const themeName in themeFilters) {
+            filterConfig.set(themeName, themeFilters[themeName]);
+        }
+        const dataStore: SDocDataStore = new SDocDataStore(new SearchParameterUtils(), filterConfig);
+        const dataService: SDocDataService = new SDocDataService(dataStore);
+
+        // configure solr-adapter
+        const sqlConfig = backendConfig['SDocSqlMytbAdapter'];
+        if (sqlConfig === undefined) {
+            throw new Error('config for SDocSqlMytbAdapter not exists');
+        }
+        const options = {
+            knexOpts: {
+                client: sqlConfig['client'],
+                connection: {
+                    host: sqlConfig['host'],
+                    user: sqlConfig['user'],
+                    password: sqlConfig['password'],
+                    database: sqlConfig['database'],
+                    port: sqlConfig['port']
+                }
+            }
+        };
+        const adapter = new SDocSqlMytbAdapter(options);
+        dataStore.setAdapter('http', adapter, '', {});
 
         return dataService;
     }
