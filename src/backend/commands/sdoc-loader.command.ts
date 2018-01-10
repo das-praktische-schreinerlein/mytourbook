729ab829dc40478c26abaa2b6e6daca2f83f51c9
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2018-01-10 22:23:33 +0100

    added sdoc-loader

---
+++ src/backend/commands/sdoc-loader.command.ts
@@ -0,0 +1,149 @@
+import {AbstractCommand} from './abstract.command';
+import * as fs from 'fs';
+import {SDocDataService} from '../shared/sdoc-commons/services/sdoc-data.service';
+import {SDocDataServiceModule} from '../modules/sdoc-dataservice.module';
+import {SDocFileUtils} from '../shared/sdoc-commons/services/sdoc-file.utils';
+import {Adapter} from 'js-data-adapter';
+import {GenericSearchOptions} from '../shared/search-commons/services/generic-search.service';
+import {Mapper, utils} from 'js-data';
+import {SDocRecord} from '../shared/sdoc-commons/model/records/sdoc-record';
+import {SDocAdapterResponseMapper} from '../shared/sdoc-commons/services/sdoc-adapter-response.mapper';
+
+export class SDocLoaderCommand implements AbstractCommand {
+    public process(argv): Promise<any> {
+        const perRun = 10;
+        const typeMapping = {
+            image: 'imageId',
+            track: 'trackId',
+            location: 'locId',
+            route: 'routeId',
+            trip: 'tripId',
+            news: 'newsId'
+        };
+        const idMappings = ['locId', 'routeId', 'trackId', 'tripId', 'newsId', 'imageId'];
+        const typeOrder = ['location', 'news', 'trip', 'route', 'track', 'image'];
+
+        const filePathConfigJson = argv['c'] || argv['backend'] || 'config/backend.json';
+        const serverConfig = {
+            backendConfig: JSON.parse(fs.readFileSync(filePathConfigJson, { encoding: 'utf8' })),
+            readOnly: false
+        };
+
+        const dataFileName = argv['f'] || argv['file'];
+        if (dataFileName === undefined) {
+            console.error('option --file expected');
+            return;
+        }
+        const recordSrcs = SDocFileUtils.parseRecordSourceFromJson(fs.readFileSync(dataFileName, { encoding: 'utf8' }));
+
+        const dataService: SDocDataService = SDocDataServiceModule.getDataService('sdocSolr' + serverConfig.readOnly,
+            serverConfig.backendConfig, serverConfig.readOnly);
+
+        const mapper: Mapper = dataService.getMapper('sdoc');
+        const adapter: Adapter = dataService.getAdapterForMapper('sdoc');
+        const responseMapper = new SDocAdapterResponseMapper();
+
+        let records = [];
+        const recordsPerType = {};
+        for (const sdocSrc of recordSrcs) {
+            const sdoc: SDocRecord = <SDocRecord>responseMapper.mapResponseDocument(mapper, sdocSrc, {});
+            const type = sdoc.type.toLowerCase();
+            if (!recordsPerType.hasOwnProperty(type)) {
+                recordsPerType[type] = [];
+            }
+            recordsPerType[type].push(sdoc);
+        }
+        for (const type of typeOrder) {
+            if (recordsPerType[type]) {
+                records = records.concat(recordsPerType[type]);
+            }
+        }
+
+        const searchOptions: GenericSearchOptions = {
+            showForm: false,
+            loadTrack: false,
+            showFacets: false
+        };
+
+        const recordIdMapping = {};
+
+        const getById = function(record: SDocRecord) {
+            const query = {
+                where: {
+                    name_s: {
+                        'in': [record.name]
+                    },
+                    type_txt: {
+                        'in': [record.type.toLowerCase()]
+                    }
+                }
+            };
+            const opts = {};
+
+            for (const idFieldName of idMappings) {
+                if (recordIdMapping[record[idFieldName]]) {
+                    console.log('map ref ' + idFieldName + ' ' + record[idFieldName]
+                        + '->' + recordIdMapping[record[idFieldName]][record[idFieldName]]);
+                    record[idFieldName] = recordIdMapping[record[idFieldName]][record[idFieldName]];
+                }
+            }
+
+            return adapter.findAll(mapper, query, opts).then(searchResult => {
+                    console.log('READ - record' + record.name, searchResult);
+                    if (!searchResult || searchResult.length <= 0) {
+                        return utils.resolve(undefined);
+                    }
+                    return utils.resolve(searchResult[0]);
+                }).then(function recordsDone(sdocRecord: SDocRecord) {
+                    console.log('RESULT - record', sdocRecord);
+                    if (sdocRecord === undefined) {
+                        console.log('ADD - record', record);
+                        return dataService.add(record);
+                    }
+
+                    console.log('EXISTING - record', record.name);
+                    return utils.resolve(sdocRecord);
+                    //console.log('UPDATE - record', record.name);
+                    //return dataService.updateById(sdocRecord.id, record);
+                }).then(function recordsDone(newSdocRecord: SDocRecord) {
+                    console.log('DONE - newrecord', newSdocRecord.id);
+                    const idFieldName = typeMapping[record.type.toLowerCase()];
+                    if (!recordIdMapping.hasOwnProperty(idFieldName)) {
+                        recordIdMapping[idFieldName] = {};
+                    }
+                    console.log('map id ' + idFieldName + ' ' + record[idFieldName] + '->' + newSdocRecord[idFieldName]);
+                    recordIdMapping[idFieldName][record[idFieldName]] = newSdocRecord[idFieldName];
+                    return utils.resolve(newSdocRecord);
+                }).catch(function searchError(error) {
+                    console.error('error thrown: ', error);
+                    return utils.reject(error);
+                }
+            );
+        };
+
+        const readUpdateOrInsert = function(start): Promise<any> {
+            console.log('RUN - load sdocs for chunk pos:' + (start + 1) + '/' + perRun);
+            const chunk = records.slice(start, start + perRun);
+            const promises = chunk.map(sdoc => {
+                return getById(sdoc);
+
+            });
+            const results = Promise.all(promises);
+
+            return results.then(data => {
+                console.log('DONE - chunk pos:' + (start + 1) + '/' + perRun);
+                if (start + perRun > recordSrcs.length) {
+                    console.log('DONE - load sdocs');
+                    return utils.resolve('WELL DONE');
+                } else {
+                    return readUpdateOrInsert(start + perRun);
+                }
+            }).catch(errors => {
+                console.error('error thrown: ', errors);
+                return utils.reject(errors);
+            });
+        };
+
+        return readUpdateOrInsert(0);
+    }
+}
--- src/backend/serverAdmin.ts
+++ src/backend/serverAdmin.ts
@@ -1,6 +1,8 @@
 import minimist from 'minimist';
 import {CacheInitializerCommand} from './commands/cache-initializer.command';
 import {SiteMapGeneratorCommand} from './commands/sitemap-generator.command';
+import {SDocLoaderCommand} from './commands/sdoc-loader.command';
+import {utils} from 'js-data';
 
 const argv = minimist(process.argv.slice(2));
 
@@ -13,14 +15,26 @@ if (!debug) {
 
 const cacheInitializer = new CacheInitializerCommand();
 const siteMapGenerator = new SiteMapGeneratorCommand();
+const sdocLoader = new SDocLoaderCommand();
 
+let promise: Promise<any>;
 switch (argv['command']) {
     case 'initCache':
-        cacheInitializer.process(argv);
+        promise = cacheInitializer.process(argv);
         break;
     case 'generateSitemap':
-        siteMapGenerator.process(argv);
+        promise = siteMapGenerator.process(argv);
+        break;
+    case 'loadSdoc':
+        promise = sdocLoader.process(argv);
         break;
     default:
         console.error('unknown command:', argv);
+        promise = utils.reject('unknown command');
 }
+
+promise.then(value => {
+    process.exit(0);
+}).catch(reason => {
+    process.exit(-1);
+});
