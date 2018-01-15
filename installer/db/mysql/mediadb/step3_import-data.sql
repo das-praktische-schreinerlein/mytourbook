34976a78dd0cc6e84f23ceb57b10ca939fbe988a
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2018-01-15 23:03:27 +0100

    added location-ancestry to model

--- installer/db/mysql/mediadb/step3_import-data.sql
+++ installer/db/mysql/mediadb/step3_import-data.sql
@@ -11,6 +11,63 @@ CREATE TABLE appids (
 
 
 -----------------
+--- functions
+-----------------
+DELIMITER $$
+DROP FUNCTION IF EXISTS `GetLocationIdAncestry` $$
+CREATE FUNCTION `GetLocationIdAncestry` (GivenID INT, pJoiner CHAR(20)) RETURNS VARCHAR(2000)
+DETERMINISTIC
+BEGIN
+    DECLARE path VARCHAR(2000);
+    DECLARE joiner CHAR(20);
+    DECLARE id INT;
+
+    SET id = GivenID;
+    SET path = concat('', id);
+    SET joiner = '';
+
+    WHILE id > 0 DO
+        SELECT IFNULL(l_parent_id,-1) INTO id FROM
+        (SELECT l_parent_id FROM location WHERE l_id = id) A;
+        IF id > 0 THEN
+            SET joiner = pJoiner;
+            SET path = CONCAT(id, joiner, path);
+        END IF;
+    END WHILE;
+    RETURN path;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS `GetLocationNameAncestry` $$
+CREATE FUNCTION `GetLocationNameAncestry` (GivenID INT, pJoiner CHAR(20)) RETURNS VARCHAR(2000)
+DETERMINISTIC
+BEGIN
+    DECLARE path VARCHAR(2000);
+    DECLARE name VARCHAR(100);
+    DECLARE joiner CHAR(20);
+    DECLARE id INT;
+
+    SET id = GivenID;
+    SET path = '';
+    SET joiner = '';
+    SET name = '';
+
+    WHILE id > 0 DO
+        SELECT l_name INTO name FROM
+        (SELECT l_name FROM location WHERE l_id = id) A;
+        IF id > 0 THEN
+            SET path = CONCAT(name, joiner, ' ', path);
+            SET joiner = pJoiner;
+        END IF;
+        SELECT IFNULL(l_parent_id,-1) INTO id FROM
+        (SELECT l_parent_id FROM location WHERE l_id = id) A;
+    END WHILE;
+    RETURN path;
+END $$
+DELIMITER ;
+
+-----------------
 --- configuration-tables
 -----------------
 DROP TABLE IF EXISTS keyword;
