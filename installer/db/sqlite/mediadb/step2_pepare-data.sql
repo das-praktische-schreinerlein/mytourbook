f25af5ca2a3c3933446763b1fc1cefac730590d4
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2018-01-31 23:00:05 +0100

    improved default-data

--- installer/db/sqlite/mediadb/step2_pepare-data.sql
+++ installer/db/sqlite/mediadb/step2_pepare-data.sql
@@ -6,17 +6,20 @@ INSERT INTO playlist (p_id, p_meta_desc, p_name) VALUES(25, 'Kalender-Favorites'
 INSERT INTO playlist (p_id, p_meta_desc, p_name) VALUES(29, 'Diashow', 'Diashow');
 
 
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(1, 'OFFEN', NULL, 1, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(658, 'Thailand', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(648, 'Spanien', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(639, 'Griechenland', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(577, 'USA', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(556, 'Schweizer Alpen', 185, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(548, 'Polen', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(460, 'Dolomiten', 185, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(449, 'Italien', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(439, 'Slowakei', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(185, 'Alpen', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(3, 'Tschechien', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(2, 'Deutschland', 1, 2, NULL, NULL, NULL);
-INSERT INTO `location` (`L_ID`, `L_NAME`, `L_PARENT_ID`, `L_TYP`, `L_GEO_LONGDEG`, `L_GEO_LATDEG`, `L_GEO_ELE`) VALUES(784, 'Türkei', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(1, 'OFFEN', NULL, 1, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(658, 'Thailand', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(648, 'Spanien', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(639, 'Griechenland', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(577, 'USA', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(556, 'Schweizer Alpen', 185, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(548, 'Polen', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(460, 'Dolomiten', 185, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(449, 'Italien', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(439, 'Slowakei', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(185, 'Alpen', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(3, 'Tschechien', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(2, 'Deutschland', 1, 2, NULL, NULL, NULL);
+INSERT INTO location (l_id, l_name, l_parent_id, l_typ, l_geo_longdeg, l_geo_latdeg, l_geo_ele) VALUES(784, 'Türkei', 1, 2, NULL, NULL, NULL);
+
+
+INSERT INTO objects (o_id, o_picasa_key, o_name, p_id, o_key) VALUES (58, '', 'Playlist_Micha', null, 'Playlist_Micha');
