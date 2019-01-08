f3e82b7ade008f1a1fdbcbb103ac2eb4103a47ec
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2019-01-08 01:01:46 +0100

    - frontend: link and shortcode for listitems to center map on item - database: updated model-scripts - backend: improved link-normalisation - model+frontend-backend: use technical name for location-filter - itemnames in links are now lowercase

--- src/shared/tdoc-commons/services/tdoc-sql-mytb.config.ts
+++ src/shared/tdoc-commons/services/tdoc-sql-mytb.config.ts
@@ -108,12 +108,12 @@ export class TourDocSqlMytbConfig {
                     noFacet: true
                 },
                 'loc_lochirarchie_txt': {
-                    selectSql: 'SELECT COUNT(*) AS count, GetTechName(l_name) AS value, l_lochirarchietxt AS label' +
+                    selectSql: 'SELECT COUNT(*) AS count, GetTechName(l_name) AS value, REPLACE(l_lochirarchietxt, ",,", " -> ") AS label' +
                     ' FROM location LEFT JOIN kategorie_full ON kategorie_full.l_id = location.l_id ' +
                     ' GROUP BY value, label' +
-                    ' ORDER BY count DESC',
-                    filterField: 'GetTechName(l_lochirarchietxt)',
-                    action: AdapterFilterActions.LIKEIN
+                    ' ORDER BY count DESC, label ASC',
+                    filterField: 'GetTechName(l_name)',
+                    action: AdapterFilterActions.IN
                 },
                 'month_is': {
                     selectField: 'MONTH(k_datevon)',
@@ -373,13 +373,13 @@ export class TourDocSqlMytbConfig {
                     noFacet: true
                 },
                 'loc_lochirarchie_txt': {
-                    selectSql: 'SELECT COUNT(*) AS count, GetTechName(l_name) AS value, l_lochirarchietxt AS label' +
+                    selectSql: 'SELECT COUNT(*) AS count, GetTechName(l_name) AS value, REPLACE(l_lochirarchietxt, ",,", " -> ") AS label' +
                     ' FROM location LEFT JOIN kategorie_full ON location.l_id = kategorie_full.l_id ' +
                     ' LEFT JOIN image ON kategorie_full.k_id=image.k_id ' +
                     ' GROUP BY value, label' +
-                    ' ORDER BY count DESC',
-                    filterField: 'GetTechName(l_lochirarchietxt)',
-                    action: AdapterFilterActions.LIKEIN
+                    ' ORDER BY count DESC, label ASC',
+                    filterField: 'GetTechName(l_name)',
+                    action: AdapterFilterActions.IN
                 },
                 'month_is': {
                     selectField: 'MONTH(i_date)',
@@ -624,12 +624,12 @@ export class TourDocSqlMytbConfig {
                     noFacet: true
                 },
                 'loc_lochirarchie_txt': {
-                    selectSql: 'SELECT COUNt(*) AS count, GetTechName(l_name) AS value, l_lochirarchietxt AS label' +
+                    selectSql: 'SELECT COUNt(*) AS count, GetTechName(l_name) AS value, REPLACE(l_lochirarchietxt, ",,", " -> ") AS label' +
                     ' FROM location LEFT JOIN tour ON tour.l_id = location.l_id ' +
                     ' GROUP BY value, label' +
-                    ' ORDER BY count DESC',
-                    filterField: 'GetTechName(l_lochirarchietxt)',
-                    action: AdapterFilterActions.LIKEIN
+                    ' ORDER BY count DESC, label ASC',
+                    filterField: 'GetTechName(l_name)',
+                    action: AdapterFilterActions.IN
                 },
                 'month_is': {
                     selectField: 'MONTH(t_datevon)',
@@ -854,12 +854,12 @@ export class TourDocSqlMytbConfig {
                 'loc_id_i': {
                 },
                 'loc_lochirarchie_txt': {
-                    selectSql: 'SELECT COUNt(*) AS count, GetTechName(l_name) AS value, l_lochirarchietxt AS label' +
+                    selectSql: 'SELECT COUNt(*) AS count, GetTechName(l_name) AS value, REPLACE(l_lochirarchietxt, ",,", " -> ") AS label' +
                     ' FROM location' +
                     ' GROUP BY value, label' +
-                    ' ORDER BY count DESC',
-                    filterField: 'GetTechName(l_lochirarchietxt)',
-                    action: AdapterFilterActions.LIKEIN
+                    ' ORDER BY count DESC, label ASC',
+                    filterField: 'GetTechName(l_name)',
+                    action: AdapterFilterActions.IN
                 },
                 'month_is': {
                     noFacet: true
