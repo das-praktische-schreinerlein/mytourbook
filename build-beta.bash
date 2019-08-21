d02a063025b6a9b5bcf0085cea9bf3ee7b98dae9
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2019-08-21 21:41:15 +0200

    - development: added configs and scripts for environments dev, dev-import, beta, prod

---
+++ build-beta.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+WORKSPACE="/cygdrive/f/Projekte/"
+MYCMSPROJECT="mytourbook_upgrade"
+
+echo "STEP: build server -> they need the node_modules"
+cd ${WORKSPACE}${MYCMSPROJECT}
+npm run backend-build || exit
+npm run frontendserver-build-server-beta-de || exit
+npm run frontendserver-build-admin-beta-de || exit
+
+echo "STEP: build frontend  -> there MUST NOT be the node_modules"
+cd ${WORKSPACE}${MYCMSPROJECT}
+npm run build-beta || exit
+npm run frontendserver-build-beta-de || exit
