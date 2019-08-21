024ed7a0c5012813b5ce2fde42097bf57570ee32
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2019-08-21 21:41:36 +0200

    - development: added configuration for environments

---
+++ devtools/configure-environment.cmd
@@ -0,0 +1,6 @@
+set DIGIFOTOS_BASEDIR=F:\playground\mytb-test\Bilder\digifotos\
+set VIDEOS_BASEDIR=F:\playground\mytb-test\Bilder\Videos\
+set MYTB_IMPORT_MEDIADIR=F:\playground\mytb-test\mytbbase\
+set MYTB_MEDIADIR=F:\playground\mytb-test\mytbmediabase\
+set DEVTOOLS=F:\Projekte\mytourbook_upgrade\devtools\
+set MYTB=F:\Projekte\mytourbook_upgrade\
