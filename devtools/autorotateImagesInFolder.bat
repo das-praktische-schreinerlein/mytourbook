a297107a60d1a8b2b4c7429c6dfc97df248cbb19
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2019-08-28 20:16:09 +0200

    - development: improved build-process with overrides

--- devtools/autorotateImagesInFolder.bat
+++
@@ -1,9 +0,0 @@
-echo off
-rem job for autorotating jpg-files
-rem requires JPEG-EXIF autorotate - http://pilpi.net/software/JPEG-EXIF_autorotate.php
-rem
-rem Example:
-rem     d:\public_projects\MatImageImportEditor\sbin\autorotateImagesInFolder.bat D:\Bilder\digifotos\test
-
-set autopath=C:\Program Files (x86)\JPEG-EXIF_autorotate\
-"%autopath%\autooperatedir_recursive.bat" "%autopath%\jhead" "%1" "%autopath%" -autorot
