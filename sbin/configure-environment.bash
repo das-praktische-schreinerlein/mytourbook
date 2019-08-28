e9c7b2e404266aeaab8212993aa84e9f448e3c9e
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2019-08-28 20:20:29 +0200

    - development: improved build-process with overrides

--- sbin/configure-environment.bash
+++ sbin/configure-environment.bash
@@ -4,5 +4,5 @@ DIGIFOTOS_BASEDIR=/cygdrive/f/playground/mytb-test/Bilder/digifotos/
 VIDEOS_BASEDIR=/cygdrive/f/playground/mytb-test//Bilder/Videos/
 MYTB_IMPORT_MEDIADIR=/cygdrive/f/playground/mytb-test/mytbbase/
 MYTB_MEDIADIR=/cygdrive/f/playground/mytb-test/mytbmediabase/
-DEVTOOLS=/cygdrive/f/Projekte/mytourbook_upgrade/devtools/
+MYTBTOOLS=/cygdrive/f/Projekte/mytourbook_upgrade/sbin/
 MYTB=/cygdrive/f/Projekte/mytourbook_upgrade/
--- sbin/configure-environment.cmd
+++ sbin/configure-environment.cmd
@@ -2,5 +2,5 @@ set DIGIFOTOS_BASEDIR=F:\playground\mytb-test\Bilder\digifotos\
 set VIDEOS_BASEDIR=F:\playground\mytb-test\Bilder\Videos\
 set MYTB_IMPORT_MEDIADIR=F:\playground\mytb-test\mytbbase\
 set MYTB_MEDIADIR=F:\playground\mytb-test\mytbmediabase\
-set DEVTOOLS=F:\Projekte\mytourbook_upgrade\devtools\
+set MYTBTOOLS=F:\Projekte\mytourbook_upgrade\sbin\
 set MYTB=F:\Projekte\mytourbook_upgrade\
