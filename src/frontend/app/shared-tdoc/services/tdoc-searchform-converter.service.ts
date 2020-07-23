44701485cd15c5d3fd4f18b78686a809274464f1
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2020-07-23 22:04:07 +0200

    - frontend: improved and fixed dashboard

--- src/frontend/app/shared-tdoc/services/tdoc-searchform-converter.service.ts
+++ src/frontend/app/shared-tdoc/services/tdoc-searchform-converter.service.ts
@@ -10,6 +10,7 @@ import {SearchFormUtils} from '@dps/mycms-frontend-commons/dist/angular-commons/
 
 @Injectable()
 export class TourDocSearchFormConverter implements GenericSearchFormSearchFormConverter<TourDocSearchForm> {
+    public readonly splitter = '_,_';
     public HRD_IDS = {
         track_id_i: 'TRACK',
         track_id_is: 'TRACK',
@@ -27,7 +28,6 @@ export class TourDocSearchFormConverter implements GenericSearchFormSearchFormCo
         destination_id_ss: 'DESTINATION',
         loc_parent_id_i: 'LOCATION'};
 
-    private splitter = '_,_';
     constructor(private searchParameterUtils: SearchParameterUtils, private translateService: TranslateService,
                 private searchFormUtils: SearchFormUtils) {
     }
