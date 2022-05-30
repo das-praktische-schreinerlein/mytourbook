fd77ef53c6d013802f9c71a3c5c692d0e35ccdd9
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2022-05-30 21:36:14 +0200

    - backend: optimized model

--- migrations/mytbdb/20220529164818-model-improvements.js
+++ migrations/mytbdb/20220529164818-model-improvements.js
@@ -21,7 +21,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', driver, '20220529164818-add-location-view-up.sql');
+  var filePath = path.join(__dirname, 'sqls', driver, '20220529164818-model-improvements-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -36,7 +36,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', driver, '20220529164818-add-location-view-down.sql');
+  var filePath = path.join(__dirname, 'sqls', driver, '20220529164818-model-improvements-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
