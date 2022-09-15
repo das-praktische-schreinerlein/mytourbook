c2b8818ac5d1adf52a77af549c6586e96178ee4d
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2022-09-15 22:10:13 +0200

    - added viewer as separate environment to view static data without backend

--- src/frontend/app/services/fallback-http-client.ts
+++ src/frontend/app/services/fallback-http-client.ts
@@ -1,14 +1,48 @@
-import {HttpClient} from '@angular/common/http';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Injectable()
 export class FallbackHttpClient {
+    private fileJsonContainer = {}
 
-    constructor(private http: HttpClient) {
+    constructor(@Inject(DOCUMENT) private document: Document) {
     }
 
     public loadJsonPData(scriptUrl: string, varName: string, dataName: string): Promise<{}> {
-        return new Promise<string>((resolve, reject) => {
+        const me = this;
+        return new Promise<{}>((resolve, reject) => {
+            let cachedJsonpSrc = this.getFromCache(scriptUrl);
+            if (cachedJsonpSrc) {
+                console.debug('SKIPPPED - loaded aleady done dataName/scriptUrl/varName from cache',
+                    dataName, scriptUrl, varName, cachedJsonpSrc);
+                try {
+                    return resolve(JSON.parse(cachedJsonpSrc));
+                } catch (error) {
+                    console.warn('SKIPPED cache - Exception while parsing ' +
+                        'jsonPData: ' + dataName + ' ' +
+                        'from ' + varName + ' ' +
+                        'url:' + scriptUrl + ': ' + error);
+                }
+            }
+
+            const element = this.document.getElementById(scriptUrl);
+            if (element) {
+                cachedJsonpSrc = element.innerHTML;
+                console.debug('SKIPPPED - loaded dataName/scriptUrl/varName from inline',
+                    dataName, scriptUrl, varName, cachedJsonpSrc);
+                try {
+                    const result = JSON.parse(cachedJsonpSrc);
+                    me.addToCache(scriptUrl, cachedJsonpSrc);
+
+                    return resolve(result);
+                } catch (error) {
+                    console.warn('SKIPPED inline-src - Exception while parsing ' +
+                        'jsonPData: ' + dataName + ' ' +
+                        'from ' + varName + ' ' +
+                        'url:' + scriptUrl + ': ' + error);
+                }
+            }
+
             const script = document.createElement('script');
             script.onload = function () {
                 if (!window[varName]) {
@@ -16,23 +50,38 @@ export class FallbackHttpClient {
                 }
 
                 const jsonpSrc = window[varName]
-                let result;
                 try {
-                    result = JSON.parse(jsonpSrc);
+                    const result = JSON.parse(jsonpSrc);
+                    me.addToCache(scriptUrl, jsonpSrc);
+
+                    return resolve(result);
                 } catch (error) {
                     return reject('Exception while parsing ' +
                         'jsonPData: ' + dataName + ' ' +
                         'from ' + varName + ' ' +
                         'url:' + scriptUrl + ': ' + error);
                 }
-
-                return resolve(result);
             };
+
             script.src = scriptUrl;
 
             document.head.appendChild(script);
         });
 
+    }
+
+    public addToCache(scriptUrl: string, data: string): void {
+        if (data === undefined) {
+            return;
+        }
+
+        this.fileJsonContainer[scriptUrl] = data;
+    }
+
+    public getFromCache(scriptUrl: string): string {
+        return this.fileJsonContainer[scriptUrl]
+            ? this.fileJsonContainer[scriptUrl]
+            : undefined;
     }
 
 }
