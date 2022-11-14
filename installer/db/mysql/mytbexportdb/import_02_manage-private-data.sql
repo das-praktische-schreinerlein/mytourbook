fc19ef5f92b872d59bd94a3e023739139196c872
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2022-11-14 22:46:12 +0100

    - backend: improved sql/solr for all entities

--- installer/db/mysql/mytbexportdb/import_02_manage-private-data.sql
+++ installer/db/mysql/mytbexportdb/import_02_manage-private-data.sql
@@ -8,4 +8,4 @@ UPDATE kategorie_full SET k_persons = CONCAT('Micha,', COALESCE(k_persons, ''));
 -- disable update of calced field to prevent long-running sqls
 --
 -- image needs 5h per 100.000 records
--- update image set l_calced_navigation_objects = '';
+-- update image set i_calced_navigation_objects = '';
