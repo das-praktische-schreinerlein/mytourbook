04a70f79e533ace47d39a5afcb84d4404a465aa9
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2023-07-02 14:36:43 +0200

    - backend/frontend: add page-management

---
+++ migrations/mytbdb/20230415111111-create-page-model.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+var driver;
+var fs = require('fs');
+var path = require('path');
+var Promise;
+
+var sql_baseDir = '../../node_modules/@dps/mycms-commons/dist/pdoc-commons/model/db';
+
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  driver = dbm.getInstance().config.getCurrent().settings.driver;
+  type = dbm.dataType;
+  seed = seedLink;
+  Promise = options.Promise;
+};
+
+exports.up = function(db) {
+  var filePath = path.join(__dirname, sql_baseDir, driver, '20230415111111-create-page-model-up.sql');
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
+
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
+
+exports.down = function(db) {
+  var filePath = path.join(__dirname, sql_baseDir, driver, '20230415111111-create-page-model-down.sql');
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
+
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
+
+exports._meta = {
+  "version": 1
+};
