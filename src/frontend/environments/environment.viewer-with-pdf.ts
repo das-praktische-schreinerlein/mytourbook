e1adb217c0157ac74ac1735b2b237efe51a244be
Michael Schreiner <michael.schreiner@your-it-fellow.de>
2024-07-14 20:49:51 +0200

    - fixed pdf-build

--- src/frontend/environments/environment.viewer-with-pdf.ts
+++ src/frontend/environments/environment.viewer-with-pdf.ts
@@ -1,9 +1,13 @@
 import {AppEnvironment} from './app-environment';
 import {environment as viewerEnvironment} from '../environments/environment.viewer';
 import {JsPdfGenerator} from '@dps/mycms-frontend-commons/dist/angular-commons/services/jspdf.generator';
+import * as html2canvas from 'html2canvas';
 
 
 export const environment: AppEnvironment = viewerEnvironment;
+
+// HINT - preload html2canvas globally to prevent chunking and lazy-loading
+window['html2canvas'] = html2canvas;
 
 // TODO if you want pdf replace PrintDialogPdfGenerator by JsPdfGenerator and move jspdf in package.json from optional to dep
 export class EnvironmentPdfGenerator extends JsPdfGenerator {}
